70eb6a83868a31ce4c83fcaeebbcb186c3e5d926
Akhitara <your-email@example.com>
2026-08-22 16:14:31 +0600

    Fix static file paths

--- flower_shop/settings.py
+++ flower_shop/settings.py
@@ -74,23 +74,15 @@ INSTALLED_APPS = [
 # =========================================================
 # MIDDLEWARE
 # =========================================================
-
 MIDDLEWARE = [
-
     'django.middleware.security.SecurityMiddleware',
-
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-
     'django.middleware.common.CommonMiddleware',
-
     'django.middleware.csrf.CsrfViewMiddleware',
-
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-
     'django.contrib.messages.middleware.MessageMiddleware',
-
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
 ]
 
 
@@ -217,16 +209,10 @@ USE_TZ = True
 # STATIC FILES
 # =========================================================
 
-STATIC_URL = 'static/'
-
-
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-
     BASE_DIR / 'static',
-
 ]
-
-
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
